9e46e44386eaa81401a3d83e8b93e82ba9efd47a
Valleryan Virgil Zuliuskandar <valleryan1212@gmail.com>
2024-05-15 02:06:31 +0700

    check

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = ['_build']
 
 # The master toctree document.
 master_doc = 'index'
